c148096b54b288307dec3f45b387677dd37f2210
EvgeniyPrivalov1986 <evprivalov86@gmail.com>
2023-04-22 16:56:56 +0500

    second_test-case

--- tests/tests_yandex_page.py
+++ tests/tests_yandex_page.py
@@ -9,9 +9,10 @@ def test_yandex_search(browser):
     yandex_main_page.enter_word("Тензор")
     yandex_main_page.check_popup_content()
     yandex_main_page.click_on_the_search_button()
+    page_url = browser.current_url
+    yandex_main_page.check_search_url(page_url)
     link = yandex_main_page.get_first_link()
-    yandex_main_page.check_link(link)
-
+    yandex_main_page.check_first_url(link)
 
 
 def test_yandex_image(browser):
@@ -19,6 +20,7 @@ def test_yandex_image(browser):
     yandex_main_page.go_to_site()
     yandex_main_page.click_on_the_menu_button()
     yandex_main_page.click_on_the_image_button()
+    image_url = browser.current_url
 
 
 
