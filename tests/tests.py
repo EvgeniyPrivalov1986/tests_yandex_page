7c4453f20f52232255392dea9ac6df06320bd8c9
EvgeniyPrivalov1986 <evprivalov86@gmail.com>
2023-04-22 11:16:49 +0500

    first test_case

--- tests/tests.py
+++ tests/tests.py
@@ -1,18 +1,19 @@
-from selenium import webdriver
-from pages.YandexPages import SearchHelper
+from pages.YandexPages import YandexPage
 import time
 
 
 def test_yandex_search(browser):
-    yandex_main_page = SearchHelper(browser)
+    yandex_main_page = YandexPage(browser)
     yandex_main_page.go_to_site()
     time.sleep(20)
     yandex_main_page.enter_word("Тензор")
     yandex_main_page.check_popup_content()
     yandex_main_page.click_on_the_search_button()
+    link = yandex_main_page.get_first_link()
+    yandex_main_page.check_link(link)
+
+
 
 def test_yandex_image(browser):
-    yandex_main_page = SearchHelper(browser)
+    yandex_main_page = YandexPage(browser)
     yandex_main_page.go_to_site()
-
-
