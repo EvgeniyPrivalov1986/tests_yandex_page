fd4d5089cb44f9c29ab3f89ddc13e6015da8023e
EvgeniyPrivalov1986 <evprivalov86@gmail.com>
2023-04-23 12:17:10 +0500

    second test-case

--- tests/tests_yandex_page.py
+++ tests/tests_yandex_page.py
@@ -20,7 +20,21 @@ def test_yandex_image(browser):
     yandex_main_page.go_to_site()
     yandex_main_page.click_on_the_menu_button()
     yandex_main_page.click_on_the_image_button()
+    browser.switch_to.window(browser.window_handles[1])
     image_url = browser.current_url
+    yandex_main_page.check_image_url(image_url)
+    yandex_main_page.click_on_the_first_category_button()
+    yandex_main_page.click_on_the_first_image()
+    time.sleep(1)
+    first_image = browser.current_url
+    yandex_main_page.click_on_the_next_image()
+    time.sleep(1)
+    next_image = browser.current_url
+    yandex_main_page.check_next_image(first_image, next_image)
+    yandex_main_page.click_on_the_previous_image()
+    time.sleep(1)
+    previous_image = browser.current_url
+    yandex_main_page.check_previous_image(previous_image, first_image)
 
 
 
